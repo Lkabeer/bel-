2cb2061ba3a23e73ff8c5b2f215f13fb9fb5e889
L kabeer <ahmed@Lkabeer.com>
2017-08-29 10:05:56 +0200

    version 4

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//$(document).ready(function() {
+//     if($(window).width() < 768) {
+//         $("#welcomeX").attr("src", "images/welcomeM.png");
+//     } else {
+//         $("#welcomeX").attr("src", "images/welcomeX.png");
+//     }
+//}); 
